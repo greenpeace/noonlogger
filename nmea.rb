a48420f527cc902ad9e60999450060a35aa11832
aib <aibok42@gmail.com>
2018-09-30 21:51:46 +0200

    adding wind force

--- nmea.rb
+++ nmea.rb
@@ -1,4 +1,3 @@
-
 # cron job line to trigger every hour
 # 55 * * * * /absolute/path/to/ruby /absolute/path/to/this/file
 
@@ -14,13 +13,15 @@ $nmeaSock = TCPSocket.new( $NMEA_SOCKET_IP, $NMEA_SOCKET_PORT )
 
 $t0 = Time.now
 $log = {}
-$noon = false
+$noon = nil
 $filename = nil
 begin
-  $tz = JSON.parse(File.read("#{$WORKING_DIR}/data/tz.json"))["timedelta"]
+  $tz = JSON.parse(File.read("#{$WORKING_DIR}/data/tz.json"))["timedelta"].to_i
 rescue
   $tz = 0
 end
+
+#puts "tz: #{$tz}"
 
 def receive_nmea
   raw = $nmeaSock.recv(4096)
@@ -34,44 +35,45 @@ def receive_nmea
       next if msg.talker == "AI"
       next if $log.has_key?(mt)
       if mt == "MWV" and msg.wind_angle_reference == "T"
-        $log["wind_direction"] = msg.wind_angle
+        units = {"K"=>0.539957,"M"=>1.94384,"N"=>1}
+        $log["wind_force"] = (msg.wind_speed * units[msg.wind_speed_units]).to_i
+        $log["wind_direction"] = msg.wind_angle.to_i
       elsif mt == "ZDA"
         local = $t0 + $tz * 3600
-        $filename = "#{local.strftime("%Y-%m-%d")}_#{$VESSEL_NAME}_NMEA.log"
-        puts "#{local.hour}:#{local.minute}"
-        $noon = true if local.hour % 24 == 11
+        $filename = "#{local.strftime("%Y-%m-%d")}_#{$VESSEL_NAME}_NMEA"
+        #puts "#{local.hour}:#{local.min}"
+        $noon = true if local.hour.to_i % 24 == 11
       elsif mt == "VTG"
-        $log["course"] = msg.track_degrees_true
+        $log["course"] = msg.track_degrees_true.to_i
       elsif mt == "GGA"
-        $log["position"] = [msg.latitude,msg.longitude]
+        $log["positionLat"] = msg.latitude
+        $log["positionLon"] = msg.longitude
       end
     rescue => e
-      puts "Parse error: #{sentence}"
-    end
-    if $log.keys.sort.join("") == "coursepositionwind_direction" and $filename
-      break
+      #puts "Parse error: #{sentence}"
+      #puts e.backtrace
     end
   end
 end
 
-if $log.keys.sort.join("") == "coursepositionwind_direction" and $filename
-  ais = JSON.parse(File.read("#{$WORKING_DIR}/data/ais.json"))
-  $log["status"] = ais["status_name"] || ""
-  $log["timestamp"] = Time.now.to_i
-  if $noon
-    File.open("#{$WORKING_DIR}/reports/#{$filename}.json","w") do |file|
-      file << $log.to_json
-    end
-  else
-    File.open("#{$WORKING_DIR}/data/position.json","w") do |file|
-      file << $log["position"].to_json
-    end
-  end
-elsif Time.now - $t0 < 120
+while $log.keys.sort.join("") != "coursepositionwind_direction" and $filename.nil? and Time.now - $t0 < 120
+  #puts "sleepin'"
+  #pp $log.keys.sort
   sleep 1
   receive_nmea
 end
 
-$nmeaSock.close
+ais = JSON.parse(File.read("#{$WORKING_DIR}/data/ais.json"))
+$log["status"] = ais["status_name"] || ""
+if $noon
+  File.open("#{$WORKING_DIR}/reports/#{$filename}.json","w") do |file|
+    file << {"NMEA"=>$log,"timestamp"=>Time.now.to_i}.to_json
+  end
+else
+  File.open("#{$WORKING_DIR}/data/position.json","w") do |file|
+    file << [$log["positionLat"],$log["positionLon"].to_json
+  end
+end
 
+$nmeaSock.close
 
